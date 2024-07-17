8ce1c9232c3d8aecb6923125464e5ff4d7a9d131
PrimeMahdi <167122910+PrimeMahdi@users.noreply.github.com>
2024-07-17 14:16:10 +0330

    Delete app.js

--- app.js
+++
@@ -1,8 +0,0 @@
-let face = document.getElementById("face");
-let input = document.getElementById("pw");
-input.addEventListener("focus",function(){
-    face.innerText = "X-X"
-})
-input.addEventListener("blur",function(){
-    face.innerText = "0-0"
-})
