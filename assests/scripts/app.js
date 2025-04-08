54345ecca8f849ef0a05d15b7b1f146213979c98
PrimeMahdi <MhdiEsfndiari@gmail.com>
2025-04-08 15:18:11 +0330

    header

--- assests/scripts/app.js
+++ assests/scripts/app.js
@@ -1,23 +1,6 @@
 let searchInput = document.querySelector('.search-input');
-let categoriesButton = document.querySelector(".categories-li");
-let categoriesList = document.querySelector(".categories-list");
-let categoriesListItems = categoriesList.children;
 window.addEventListener("keypress",(e)=>{
     if(e.key === "o"){
         searchInput.focus();
     }
 });
-categoriesButton.addEventListener("mouseenter",()=>{
-    categoriesList.style.transform = "translateY(0)";
-});
-categoriesButton.addEventListener("mouseleave",()=>{
-    categoriesList.style.transform = "translateY(-200px)";
-    console.log("leaved");
-});
-categoriesList.addEventListener("mouseenter",()=>{
-    categoriesList.style.transform = "translateY(0)";
-});
-categoriesList.addEventListener("mouseleave",()=>{
-    console.log("leaved");
-    categoriesList.style.transform = "translateY(-200px)";
-}); 
